55be7b8bd9e10cb66c6366c6d0a22219aef4c72e
ericnoam <eric.rodriguez@leovegas.com>
2024-02-18 21:09:52 +0100

    initial commit

---
+++ starter.py
@@ -0,0 +1,27 @@
+import os.path
+import logging
+import sys
+
+import os.path
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+PERSIST_DIR="./storage"
+if not os.path.exists(PERSIST_DIR):
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do growing up?")
+print(response)
